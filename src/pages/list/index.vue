8bf2396ce5325100cbf44bf4bdbe41185daa75ec
SnowyLay <2650901980@qq.com>
2019-12-23 23:18:22 +0800

    框架

---
+++ src/pages/list/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+  <i-grid>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>歌曲</i-grid-label>
+    </i-grid-item>    
+    <i-grid-item>
+        <i-grid-icon>
+          
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>影视</i-grid-label>
+    </i-grid-item>
+        <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>综艺</i-grid-label>
+    </i-grid-item>
+        <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>行程</i-grid-label>
+    </i-grid-item>
+  </i-grid>   
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+
+  methods: {
+    },
+   
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+</style>
