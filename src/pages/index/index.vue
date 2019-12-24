fe58d3793b10c7b0bf57444cff8af285f2555064
SnowyLay <2650901980@qq.com>
2019-12-24 21:19:54 +0800

    所有页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,42 @@
 <template>
   <div>
-    
+     <swiper
+      :indicator-dots="indicatorDots"
+      :autoplay="autoplay"
+      :interval="interval"
+      :duration="duration"
+      style="height:200px"
+    >
+    <block v-for="item in imgUrls" :key="item">
+      <swiper-item>
+        <image :src="item" style="width:100%;"/>
+      </swiper-item>
+    </block>
+  </swiper>
+  <i-panel title="成员简介">
+    <view style="padding: 15px;">
+      <i-card i-class="split" title="卡片标题" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <i-card i-class="split" title="卡片标题" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <i-card i-class="split" title="卡片标题" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <i-card i-class="split" title="卡片标题" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <i-card i-class="split" title="卡片标题" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+    </view>
+</i-panel>
   </div>
 </template>
 
@@ -8,6 +44,15 @@
 export default {
   data () {
     return {
+      imgUrls: [
+        '/static/images/团体照3.jpg',
+        '/static/images/团体照2.jpg',
+        '/static/images/团体照1.jpg'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 1000,
     }
   },
 
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <view class="userinfo">
+    <view class="userinfo-avatar">
+    <open-data type="userAvatarUrl"></open-data>
+    </view>
+    <open-data type="userNickName"></open-data>
+    </view>
+    <i-panel title="我是粉丝，我要安利！！！">
+      <i-input :value="name" @change="changeName($event)" title="作品名称" autofocus placeholder="请输入店名" maxlength="20" />
+      <i-input :value="time" @change="changeTime($event)" title="详细地址" placeholder="请输入详细地址" maxlength="30" />
+      <i-input :value="reason" @change="changeReason($event)" title="推荐理由" placeholder="请输入推荐理由" maxlength="50" />
+    </i-panel>
+    <i-button @click="handleClick" type="warning" size="default">我要推荐</i-button>
+    <view class="tips">湖北大学吃货专用小程序，如果你发现附近有好吃的请分享给我们！！！</view>
   </div>
 </template>
 
@@ -7,18 +20,68 @@
 export default {
   data () {
     return {
+      name:"",
+      time:"",
+      reason:""
     }
   },
-
-
   methods: {
+    changeName (event) {
+      this.name = event.mp.detail.detail.value
     },
-   
-
+    changeReason (event) {
+      this.reason = event.mp.detail.detail.value
+    },
+    changeTime (event) {
+      this.time = event.mp.detail.detail.value
+    },
+    handleClick () {
+      if (this.name && this.reason && this.time) {
+        wx.showToast({
+          title: '推荐了' + this.name,
+          icon: 'success',
+          duration: 2000
+        })
+        // TODO:将推荐数据提交到云数据库
+      } else {
+        wx.showToast({
+          title: '信息不完整',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    }
+  },
   created () {
   }
 }
 </script>
 
 <style scoped>
+.userinfo {
+  position: relative;
+  width: 750rpx;
+  height: 320rpx;
+  color: #666;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+ 
+.userinfo-avatar {
+  overflow:hidden;
+  display: block;
+  width: 160rpx;
+  height: 160rpx;
+  margin: 20rpx;
+  margin-top: 50rpx;
+  border-radius: 50%;
+  border: 2px solid #fff;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
+.tips {
+  padding: 20pt;
+  font-size: 10pt;
+  color:#c078ff;
+}
 </style>
